cf9ef016bd220ccf21354f6b48acd276453e0338
SheaParrott <alexandergotthis@gmail.com>
2018-12-02 18:10:30 -0500

    basic layout of home page created with data given

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+import gallery from './gallery.json'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      lyrics: '',
+      pandaImage: '',
+      pandaParagraph: '',
+      miniatureImage: '',
+      miniatureParagraph: ''
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header>Photo Gallery</header>
+        <div className="headerMenu">
+          <a href="#">home</a>
+        </div>
+        <main>
+          <div className="imageBox">
+            <a
+              href="#"
+              style={{
+                textDecoration: 'none'
+              }}
+            >
+              {gallery.pandas.title}
+            </a>
+            <p>{gallery.pandas.description}</p>
+            <img
+              src={gallery.pandas.photos[0].imageURL}
+              alt={gallery.pandas.title}
+            />
+          </div>
+          <div className="imageBox">
+            <a
+              href="#"
+              style={{
+                textDecoration: 'none'
+              }}
+            >
+              {gallery.miniatures.title}
+            </a>
+            <p>{gallery.miniatures.description}</p>
+            <img
+              src={gallery.miniatures.photos[0].imageURL}
+              alt={gallery.miniatures.title}
+            />
+          </div>
+        </main>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
